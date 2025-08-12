9023c32b48c339071225965f3c3a6c42b641e090
Rodrigo Kennes <rodrigo.kennes@gmail.com>
2025-08-12 14:25:01 -0300

    configs to deploy in railway

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
+from dotenv import load_dotenv
 
-SECRET = os.getenv("JWT_SECRET", "ticketsforfootball")  
+load_dotenv()  
+
+SECRET = os.getenv("JWT_SECRET", "ticketsforfootball")
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 JWT_ISSUER = "ticketsforfootball.com.br"
 JWT_AUDIENCE = "ticketsforfootball"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SYNC_DATABASE_URL = os.getenv("SYNC_DATABASE_URL")
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "local") 
